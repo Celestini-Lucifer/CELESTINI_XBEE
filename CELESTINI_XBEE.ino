de51ed79eb548e94fdea66a47b32266c00720e6c
naughtyStark <sidharthtalia@ymail.com>
2018-05-13 22:05:35 +0530

    added the OG versions of the xbee files.

---
+++ CELESTINI_XBEE.ino
@@ -0,0 +1,53 @@
+#include <XBee.h>
+#include<SoftwareSerial.h>
+
+#define nssRX 2
+#define nssTX 3
+
+SoftwareSerial nss(nssRX,nssTX);
+
+XBee xbee = XBee();//constructor?
+XBeeResponse response = XBeeResponse();//also constructor?
+
+// create reusable response objects for responses we expect to handle 
+uint8_t option = 0;
+uint8_t DataOut[2];//data that we will receive,for now, its like just 2 bytes for testing purposes.
+uint8_t DataIn[2];//data received. 
+
+
+uint8_t idCmd[] = {'I','D'};
+//uint8_t idValue[] = {0x01, 0x10}; // some made up value for testing purposes
+uint8_t idCmd2[] = {'W','R'}; // write to EEPROM command.
+
+void setup() 
+{
+  // start serial
+  nss.begin(9600);
+  xbee.setSerial(nss);
+  Serial.begin(38400);
+  delay(15000);//xbee's are cray cray. they need some time to start.
+}
+
+// continuously reads packets, looking for RX16 or RX64
+void loop()
+{   
+    Read(); //read the environment.
+    DataOut[1] = 255;
+    DataOut[0] = 128;//just some random shit
+    BroadCast();//broadcast   
+}
+
+void reset_ID(uint8_t ID)
+{
+    uint8_t *idValue = &ID;
+    
+    AtCommandRequest request = AtCommandRequest(idCmd, idValue, sizeof(idValue));
+    
+    sendAtCommand(request);
+    request.clearCommandValue();
+    request.setCommand(idCmd2);
+    sendAtCommand(request);
+    request.clearCommandValue();
+}
+
+
---
+++ read_BroadCast_setup.ino
@@ -0,0 +1,75 @@
+//this code was written while listening to "Arctic monkeys- Do I wanna know?"
+void BroadCast()
+{
+    TxStatusResponse txStatus = TxStatusResponse();//may not actually need this.
+    Tx16Request tx = Tx16Request(0xFFFF, DataOut, sizeof(DataOut)); //format of the frame being broadcasted. destination address,payload,size of payload
+    xbee.send(tx);//send out that info brotha. broadcast has no acknowledgement bit so i guess this is pretty much it (fingers crossed).
+}
+
+void Read()//function to read stuff
+{
+    Rx16Response rx16 = Rx16Response(); //objects for doing the response related stuff.
+    Rx64Response rx64 = Rx64Response();
+    
+    xbee.readPacket();//anything available out there?
+    if (xbee.getResponse().isAvailable())//WE GOT ONE!! 
+    {    
+      if (xbee.getResponse().getApiId() == RX_16_RESPONSE) //if its using 16 bit addressing. Not sure how this stuff works in broadcast mode. although i guess it still works 
+      {
+        xbee.getResponse().getRx16Response(rx16);//take the response, dump it in an object by the name of rx16
+        option = rx16.getOption();//why do i need this?
+        DataIn[0] = rx16.getData(0);//the thing we care 'bout
+        DataIn[1] = rx16.getData(1);
+      } 
+      else if(xbee.getResponse().getApiId() == RX_64_RESPONSE)//don't got no clue why i am doing this
+      {
+        xbee.getResponse().getRx64Response(rx64);//not commenting this stuff cuz its the same as for 16 bit
+        option = rx64.getOption();//why do i need this?
+        DataIn[0] = rx64.getData(0);//the thing we care 'bout
+        DataIn[1] = rx64.getData(1);        
+      } 
+      else //(error)
+      {
+
+      }
+    } 
+    else if (xbee.getResponse().isError())
+    {
+
+    } 
+}
+
+void sendAtCommand(AtCommandRequest request) 
+{
+  AtCommandResponse response = AtCommandResponse();
+  xbee.send(request);
+  if (xbee.readPacket(5000)) 
+  {
+    if (xbee.getResponse().getApiId() == AT_COMMAND_RESPONSE)
+    {
+      xbee.getResponse().getAtCommandResponse(response);
+
+      if (response.isOk()) //Are you okay? I isOk.
+      {
+
+      } 
+      else//command returned some error code for some godforsaken reason.
+      {
+
+        
+      }
+    } 
+    else//something went haywire
+    {
+
+    }    
+  } 
+  else if (xbee.getResponse().isError()) //Houston, we have a problem
+  {
+
+  }
+  else//no response from radio 
+  {
+
+  }
+}
